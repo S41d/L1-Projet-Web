f035f55620ebc4c1b34c76dfac843612e8de2a0b
Saïd Mohammad Zuhair <m.masoud247@gmail.com>
2020-03-11 15:43:54 +0100

    barre de recherche

--- Accueil/script.js
+++ Accueil/script.js
@@ -1,4 +1,15 @@
+let searchinput, style;
 function bar_de_recherche() {
-    let searchinput = document.getElementById('inputrecherche')
-    searchinput.style.display = "block"    
+    searchinput = document.getElementById('barderechercher')
+    style = window.getComputedStyle(searchinput);
+    console.log(style.width)
+    if (style.width === "0px"){
+        searchinput.style.width = "240px"
+        searchinput.style.paddingLeft = "20px"
+        searchinput.classList.add('search-open')
+    }
+    else {
+        searchinput.style.width = "0px"
+        searchinput.style.paddingLeft = "5px"
+    }   
 }
